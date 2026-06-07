0dfd1d7ca3711184c8477d90bcbfd9c6355b10cb
Miaauwcorp <simibsriau@gmail.com>
2026-06-07 14:05:28 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = "sim-murojaah-ibs-v1";
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys
+          .filter(function (key) {
+            return key !== CACHE_NAME;
+          })
+          .map(function (key) {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  const request = event.request;
+
+  if (request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(request).then(function (cached) {
+      return cached || fetch(request).catch(function () {
+        if (request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+      });
+    })
+  );
+});
